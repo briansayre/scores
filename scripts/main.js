c8ba587f7683ec00b28e5cf5be057b464ac13c1e
Brian Sayre <briansayre01@gmail.com>
2024-08-27 20:07:41 -0500

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,12 @@
-function setCookie(cname, cvalue, exdays) {
+// set a cookie value
+function setCookie(cName, cValue, expirationDays) {
     const d = new Date();
-    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+    d.setTime(d.getTime() + expirationDays * 24 * 60 * 60 * 1000);
     let expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    document.cookie = cName + "=" + cValue + ";" + expires + ";path=/";
 }
 
+// get a cookie value
 function getCookie(cname) {
     let name = cname + "=";
     let ca = document.cookie.split(";");
@@ -20,6 +22,7 @@ function getCookie(cname) {
     return "";
 }
 
+// convert number to quarter string
 function numToQuart(num) {
     if (num === 4) return "4th";
     if (num === 3) return "3rd";
@@ -29,28 +32,29 @@ function numToQuart(num) {
     return "OT";
 }
 
+// are both teams ranked
 function bothRanked(game) {
-    return game.home.rank !== "" && game.away.rank !== "";
+    return (game.home.rank !== "" && game.away.rank !== "");
 }
 
-function bothHighWinPerc(game) {
-    var homeIndexOf = game.home.record.indexOf("-");
-    if (homeIndexOf === -1) return false;
-    var homeWins = Number(game.home.record.substring(0, homeIndexOf));
-    var homeLosses = Number(game.home.record.substring(homeIndexOf + 1));
-    if (homeWins + homeLosses === 0) return false;
-    if (homeWins / (homeWins + homeLosses) < 0.666) return false;
-
-    var awayIndexOf = game.away.record.indexOf("-");
-    if (awayIndexOf === -1) return false;
-    var awayWins = Number(game.away.record.substring(0, awayIndexOf));
-    var awayLosses = Number(game.home.record.substring(awayIndexOf + 1));
-    if (awayWins + awayLosses === 0) return false;
-    if (awayWins / (awayWins + awayLosses) < 0.666) return false;
-
-    return true;
+// get a teams win percentage
+function getWinPercent(team) {
+    var indexOfDash = team.record.indexOf("-");
+    if (indexOfDash === -1) return -1;
+    var wins = Number(team.record.substring(0, indexOfDash));
+    var losses = Number(team.record.substring(indexOfDash + 1));
+    var total = wins + losses
+    if (total === 0) return -1;
+    return wins / total;
 }
 
+// do both teams have high win percentage
+function bothHighWinPercent(game) {
+    var threshold = .66;
+    return (getWinPercent(game.away) > threshold && getWinPercent(game.home) > threshold);
+}
+
+// is the game close towards the end
 function isCloseGame(game) {
     return (
         Math.abs(Number(game.home.score) - Number(game.away.score)) < 9 &&
@@ -58,23 +62,26 @@ function isCloseGame(game) {
     );
 }
 
+// does the game contain a favorite team
 function containsFavorite(game) {
     if (!game.isNfl && (game.home.id === "66" || game.away.id === "66")) return true;
     if (game.isNfl && (game.home.id === "6" || game.away.id === "6")) return true;
     return false;
 }
 
-function renderDate(value, className) {
+// render a date or time
+function renderDateOrTime(value, className) {
     var htmlObject = document.createElement("div");
     htmlObject.classList.add(className);
     htmlObject.innerHTML = value;
     document.getElementById("container").appendChild(htmlObject);
 }
 
+// given a game, render the div
 function renderGame(game) {
     var htmlObject = document.createElement("div");
-    htmlObject.classList.add("game");
 
+    htmlObject.classList.add("game");
     if (game.isClose) {
         htmlObject.classList.add("close-game");
     } else if (game.isGood) {
@@ -87,122 +94,124 @@ function renderGame(game) {
         window.open(game.link);
     };
 
-    var string = `
-	<div class="network">
-		<i class="${game.live}"></i>${game.topHeader}
-	</div>
-	<div class="scores">
-		<div class="teams">
-			<div class="team away">
-				<div class="team-logo">
-					<svg width="32" height="32" class="${game.ncaaIcon}">
-						<rect class="logo-background" x="0" y="0" rx="3" ry="3" width="32" height="32" fill="#${game.away.primary}"/>
-						<rect class="stripe" x="12" y="0" width="8" height="32" fill="#${game.away.alternate}"/>
-						<rect class="stripe" x="9" y="0" width="3" height="32" fill="white"/>
-						<rect class="stripe" x="20" y="0" width="3" height="32" fill="white"/>
-					</svg>
-					<svg width="32" height="32" class="${game.nflIcon}">
-						<rect class="logo-background" x="0" y="0" rx="3" ry="3" width="32" height="32" fill="#${game.away.primary}"/>
-						<circle class="stripe"  cx="16" cy="16" r="8" fill="#${game.away.alternate}" stroke="white" stroke-width="3"/>
-					</svg>
-				</div>
-				<div class="team-name">
-					${game.away.name} <span class="rank">${game.away.rank}</span>
-					<div class="team-info">
-						${game.away.record} ${game.away.downAndDist}
-					</div>
-				</div>
-				<div class="score-icons">
-					<div class="timeouts">
-						<svg class="${game.away.timeouts[2]}" width="6" height="6">
-							<circle cx="3" cy="3" r="3" />
-						</svg>
-						<svg class="${game.away.timeouts[1]}" width="6" height="6">
-							<circle cx="3" cy="3" r="3" />
-						</svg>
-						<svg class="${game.away.timeouts[0]}" width="6" height="6">
-							<circle cx="3" cy="3" r="3" />
-						</svg>
-					</div>
-					<div class="possession">
-						<svg class="${game.away.possession}" width="10" height="6">
-							<ellipse cx="5" cy="3" rx="5" ry="3" />
-						</svg>
-					</div>
-				</div>
-				<div class="score">
-					${game.away.score}
-				</div>
-			</div>
-			<div class="team home">
-				<div class="team-logo">
-					<svg width="32" height="32" class="${game.ncaaIcon}">
-						<rect class="logo-background" x="0" y="0" rx="3" ry="3" width="32" height="32" fill="#${game.home.primary}"/>
-						<rect class="stripe" x="12" y="0" width="8" height="32" fill="#${game.home.alternate}"/>
-						<rect class="stripe" x="9" y="0" width="3" height="32" fill="white"/>
-						<rect class="stripe" x="20" y="0" width="3" height="32" fill="white"/>
-					</svg>
-					<svg width="32" height="32" class="${game.nflIcon}">
-						<rect class="logo-background" x="0" y="0" rx="3" ry="3" width="32" height="32" fill="#${game.home.primary}"/>
-						<circle class="stripe"  cx="16" cy="16" r="8" fill="#${game.home.alternate}" stroke="white" stroke-width="3" />
-					</svg>
-				</div>
-				<div class="team-name">
-					${game.home.name} <span class="rank">${game.home.rank}</span>
-					<div class="team-info">
-						${game.home.record} ${game.home.downAndDist}
-					</div>
-				</div>
-				<div class="score-icons">
-					<div class="timeouts">
-						<svg class="${game.home.timeouts[2]}" width="6" height="6">
-							<circle cx="3" cy="3" r="3" />
-						</svg>
-						<svg class="${game.home.timeouts[1]}" width="6" height="6">
-							<circle cx="3" cy="3" r="3" />
-						</svg>
-						<svg class="${game.home.timeouts[0]}" width="6" height="6">
-							<circle cx="3" cy="3" r="3" />
-						</svg>
-					</div>
-					<div class="possession">
-						<svg class="${game.home.possession}" width="10" height="6">
-							<ellipse cx="5" cy="3" rx="5" ry="3" />
-						</svg>
-					</div>
-				</div>
-				<div class="score">
-					${game.home.score}
-				</div>
-			</div>
-		</div>
-		<div class="progress">
-			<div class="time">
-				${game.time}
-			</div>
-			<div class="quarter">
-				${game.quarter}
-			</div>
-		</div>
-	</div>
-	<div class="last-play">
-		${game.lastPlay}
-	</div>
-`;
-    htmlObject.innerHTML = string;
+    var htmlString = `
+        <div class="network">
+            <i class="${game.live}"></i>${game.topHeader}
+        </div>
+        <div class="scores">
+            <div class="teams">
+                <div class="team away">
+                    <div class="team-logo">
+                        <svg width="32" height="32" class="${game.ncaaIcon}">
+                            <rect class="logo-background" x="0" y="0" rx="3" ry="3" width="32" height="32" fill="#${game.away.primary}"/>
+                            <rect class="stripe" x="12" y="0" width="8" height="32" fill="#${game.away.alternate}"/>
+                            <rect class="stripe" x="9" y="0" width="3" height="32" fill="white"/>
+                            <rect class="stripe" x="20" y="0" width="3" height="32" fill="white"/>
+                        </svg>
+                        <svg width="32" height="32" class="${game.nflIcon}">
+                            <rect class="logo-background" x="0" y="0" rx="3" ry="3" width="32" height="32" fill="#${game.away.primary}"/>
+                            <circle class="stripe"  cx="16" cy="16" r="8" fill="#${game.away.alternate}" stroke="white" stroke-width="3"/>
+                        </svg>
+                    </div>
+                    <div class="team-name">
+                        ${game.away.name} <span class="rank">${game.away.rank}</span>
+                        <div class="team-info">
+                            ${game.away.record} ${game.away.downAndDist}
+                        </div>
+                    </div>
+                    <div class="score-icons">
+                        <div class="timeouts">
+                            <svg class="${game.away.timeouts[2]}" width="6" height="6">
+                                <circle cx="3" cy="3" r="3" />
+                            </svg>
+                            <svg class="${game.away.timeouts[1]}" width="6" height="6">
+                                <circle cx="3" cy="3" r="3" />
+                            </svg>
+                            <svg class="${game.away.timeouts[0]}" width="6" height="6">
+                                <circle cx="3" cy="3" r="3" />
+                            </svg>
+                        </div>
+                        <div class="possession">
+                            <svg class="${game.away.possession}" width="10" height="6">
+                                <ellipse cx="5" cy="3" rx="5" ry="3" />
+                            </svg>
+                        </div>
+                    </div>
+                    <div class="score">
+                        ${game.away.score}
+                    </div>
+                </div>
+                <div class="team home">
+                    <div class="team-logo">
+                        <svg width="32" height="32" class="${game.ncaaIcon}">
+                            <rect class="logo-background" x="0" y="0" rx="3" ry="3" width="32" height="32" fill="#${game.home.primary}"/>
+                            <rect class="stripe" x="12" y="0" width="8" height="32" fill="#${game.home.alternate}"/>
+                            <rect class="stripe" x="9" y="0" width="3" height="32" fill="white"/>
+                            <rect class="stripe" x="20" y="0" width="3" height="32" fill="white"/>
+                        </svg>
+                        <svg width="32" height="32" class="${game.nflIcon}">
+                            <rect class="logo-background" x="0" y="0" rx="3" ry="3" width="32" height="32" fill="#${game.home.primary}"/>
+                            <circle class="stripe"  cx="16" cy="16" r="8" fill="#${game.home.alternate}" stroke="white" stroke-width="3" />
+                        </svg>
+                    </div>
+                    <div class="team-name">
+                        ${game.home.name} <span class="rank">${game.home.rank}</span>
+                        <div class="team-info">
+                            ${game.home.record} ${game.home.downAndDist}
+                        </div>
+                    </div>
+                    <div class="score-icons">
+                        <div class="timeouts">
+                            <svg class="${game.home.timeouts[2]}" width="6" height="6">
+                                <circle cx="3" cy="3" r="3" />
+                            </svg>
+                            <svg class="${game.home.timeouts[1]}" width="6" height="6">
+                                <circle cx="3" cy="3" r="3" />
+                            </svg>
+                            <svg class="${game.home.timeouts[0]}" width="6" height="6">
+                                <circle cx="3" cy="3" r="3" />
+                            </svg>
+                        </div>
+                        <div class="possession">
+                            <svg class="${game.home.possession}" width="10" height="6">
+                                <ellipse cx="5" cy="3" rx="5" ry="3" />
+                            </svg>
+                        </div>
+                    </div>
+                    <div class="score">
+                        ${game.home.score}
+                    </div>
+                </div>
+            </div>
+            <div class="progress">
+                <div class="time">
+                    ${game.time}
+                </div>
+                <div class="quarter">
+                    ${game.quarter}
+                </div>
+            </div>
+        </div>
+        <div class="last-play">
+            ${game.lastPlay}
+        </div>
+    `;
+    htmlObject.innerHTML = htmlString;
     document.getElementById("container").appendChild(htmlObject);
 }
 
-function getTeam(isAway, game, gameFormatted) {
-    var t = game.competitors[isAway];
+// parse the team from the response
+function getTeam(isAway, comp, game) {
+    var t = comp.competitors[isAway];
     var team = {};
-    var state = gameFormatted.state;
+    var state = game.state;
 
     team.id = t.id;
     team.name = t.team.shortDisplayName;
     team.record = t.records !== undefined ? t.records[0].summary : "";
     team.possession = "hide";
     team.timeouts = ["hide", "hide", "hide"];
+    team.timeoutCount = 3; // TODO: get timeout number
     team.score = t.score;
     team.rank = t.curatedRank !== undefined ? t.curatedRank.current : "";
     team.rank = team.rank !== 99 ? team.rank : "";
@@ -215,12 +224,13 @@ function getTeam(isAway, game, gameFormatted) {
     if (state == "pre") {
         team.score = "-";
     } else if (state == "post") {
+
     } else {
-        if (team.id == gameFormatted.possession) {
-            team.downAndDist = gameFormatted.downAndDist;
+        if (team.id == game.possession) {
+            team.downAndDist = game.downAndDist;
             team.possession = "show";
         }
-        for (let i = 0; i < 3; i++) {
+        for (let i = 0; i < team.timeoutCount; i++) {
             team.timeouts[i] = "show";
         }
     }
@@ -228,27 +238,21 @@ function getTeam(isAway, game, gameFormatted) {
     return team;
 }
 
-function getGame(g, isNfl) {
+// parse the game from the response
+function getGame(event, isNfl) {
     var game = {};
     try {
-        var comp = g.competitions !== undefined ? g.competitions[0] : {};
-        if (comp == {}) {
-            return;
-        }
+        var comp = event.competitions !== undefined ? event.competitions[0] : {};
+        if (comp == {}) return;
 
-        game.state = g.status !== undefined ? g.status.type.state : "pre";
-        game.link = g.links !== undefined ? g.links[0].href : "";
+        game.state = event.status !== undefined ? event.status.type.state : "pre";
+        game.link = event.links !== undefined ? event.links[0].href : "";
         game.venue = comp.venue.fullName;
-        game.date = new Date(comp.startDate !== undefined ? comp.startDate : g.date);
-
-        game.dateString =
-            game.date.toLocaleDateString([], { weekday: "long" }) +
-            " " +
-            game.date.toLocaleDateString([], { month: "numeric", day: "numeric" });
+        game.date = new Date(comp.startDate !== undefined ? comp.startDate : event.date);
+        game.dateString = game.date.toLocaleDateString([], { weekday: "long", month: "numeric", day: "numeric" })
         game.timeString = game.date.toLocaleTimeString([], { timeStyle: "short" });
-        game.startDate = game.dateString + " - " + game.timeString;
         game.channel = comp.broadcasts[0].names !== undefined ? comp.broadcasts[0].names.join("/") : "";
-        game.odds = comp.odds !== undefined ? " (" + comp.odds[0].details + " O/U " + comp.odds[0].overUnder + ")" : "";
+        game.odds = comp.odds !== undefined ? " (" + comp.odds[0].details + (comp.odds[0].overUnder !== undefined ? " O/U " + comp.odds[0].overUnder + ")" : ")") : "";
         game.topHeader = [game.channel, game.venue, game.odds].filter(Boolean).join(" - ");
         game.lastPlay = " ";
         game.downAndDist = " ";
@@ -272,8 +276,8 @@ function getGame(g, isNfl) {
             game.time = " ";
             game.quarter = "Final";
         } else {
-            game.time = g.status.displayClock;
-            game.quarter = numToQuart(g.status.period);
+            game.time = event.status.displayClock;
+            game.quarter = numToQuart(event.status.period);
             game.live = "fa fa-circle text-danger-glow blink";
             console.log(g);
         }
@@ -283,13 +287,14 @@ function getGame(g, isNfl) {
 
         if (isCloseGame(game)) {
             game.isClose = true;
-        } else if (bothRanked(game) || bothHighWinPerc(game)) {
+        } else if (bothRanked(game) || bothHighWinPercent(game)) {
             game.isGood = true;
         } else if (containsFavorite(game)) {
             game.isFavorite = true;
         }
+
     } catch (err) {
-        throw err;
+        console.log(err);
     }
 
     return game;
@@ -298,8 +303,8 @@ function getGame(g, isNfl) {
 var games = [];
 var requests = [];
 var extraGameRequests = [];
-var extraTeams = [66, 38, 2460, 2294];
 var extraGames = [];
+var extraTeams = [66, 38, 2460, 2294];
 
 // ncaa
 requests.push(
@@ -355,8 +360,9 @@ for (let i = 0; i < extraTeams.length; i++) {
     );
 }
 
-// when ncaa, nfl, and ectra teams are done, do extra games
+// when ncaa, nfl, and extra teams are done, do extra games
 $.when.apply($, requests).done(function () {
+    // get extra games
     for (let i = 0; i < extraGames.length; i++) {
         extraGameRequests.push(
             $.ajax({
@@ -374,7 +380,7 @@ $.when.apply($, requests).done(function () {
             })
         );
     }
-
+    // now render the games
     $.when.apply($, extraGameRequests).done(function () {
         games.sort(function (a, b) {
             return a.date - b.date;
@@ -385,11 +391,11 @@ $.when.apply($, requests).done(function () {
 
         for (let i = 0; i < games.length; i++) {
             if (games[i].dateString !== prevDate) {
-                renderDate(games[i].dateString, "game-date");
+                renderDateOrTime(games[i].dateString, "game-date");
                 prevDate = games[i].dateString;
             }
             if (games[i].timeString !== prevTime) {
-                renderDate(games[i].timeString, "game-time");
+                renderDateOrTime(games[i].timeString, "game-time");
                 prevTime = games[i].timeString;
             }
             renderGame(games[i]);
@@ -399,11 +405,11 @@ $.when.apply($, requests).done(function () {
             document.getElementById("container").style.visibility = "visible";
             document.getElementById("loading").style.display = "none";
 
-            var scrollpos = getCookie("scrollpos");
-            if (scrollpos) window.scrollTo(0, scrollpos);
+            var scrollPos = getCookie("scrollPos");
+            if (scrollPos) window.scrollTo(0, scrollPos);
 
             window.onbeforeunload = function (e) {
-                setCookie("scrollpos", window.scrollY, 14);
+                setCookie("scrollPos", window.scrollY, 14);
             };
         });
     });
